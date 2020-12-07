9acb97926413ab1831ff7335b41019cdc51a9c6a
Patrick Zhang <patricktjuatja@yahoo.com>
2020-12-07 20:17:45 +0700

    Admin

---
+++ resources/views/template/index.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-green-admin bg-green">
+  <a class="navbar-brand-admin" href="{{ route('admin.panel') }}" >Admin</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+    <div class="collapse navbar-collapse text-right" id="navbarToggler">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    Product
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('admin.addproduct') }}">
+                        Add
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        Show All
+                    </a>
+                </div>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    Category
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('admin.addcategory') }}">
+                        Add
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        Show all
+                    </a>
+                </div>
+            </li>
+        </ul>        
+        
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('admin.logout') }}"
+                                onclick="clickLogOut()">
+                                Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                </div>
+            </li>  
+        </ul>                    
+  </div>
+</nav>
+
+@yield('container')
+
+</body>
+
+</html>
