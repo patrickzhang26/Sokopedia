ec32f0eb54ff24ee25efb734f39580a715063875
Patrick Zhang <patricktjuatja@yahoo.com>
2020-12-08 21:59:35 +0700

    register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,6 +69,6 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'role'=>'user',
-        ])->with('success','You are registered in $okopedia.');
+        ]);
     }
 }
