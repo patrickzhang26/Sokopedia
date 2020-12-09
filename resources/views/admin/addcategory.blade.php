a896e858bb0f692574543f45c060ad472daa77dd
Apriliando <apriliandodo@gmail.com>
2020-12-09 17:56:02 +0700

    hilangin tag center, perbaikan blade listcategory & controller-nya

--- resources/views/admin/addcategory.blade.php
+++ resources/views/admin/addcategory.blade.php
@@ -3,19 +3,23 @@
 @section('title','$okopedia - Add Category')
 
 @section('container')
-<center>
-    <div class="admin_content">
-        <form>
-            <div class="form-group">
-                @csrf
-                <h3>Add Category</h3>
-                <p>Name</p>
-                <input class="form-control-admin" type="text" placeholder="Category Name"><br>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="addproduct">Add Category</button>
+    <div class="container h-100">
+        <div class="row align-items-center h-100">
+            <div class="col-10 mx-auto">
+                <div class="admin_content">
+                    <form>
+                        <div class="form-group">
+                            @csrf
+                            <h3>Add Category</h3>
+                            <b>Name</b>
+                            <input class="form-control-admin" type="text" placeholder="Category Name"><br>
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="addproduct">Add Category</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </form>
+        </div>
     </div>
-</center>
 @endsection
 
 </body>
--- resources/views/admin/listproduct.blade.php
+++ resources/views/admin/listproduct.blade.php
@@ -3,37 +3,41 @@
 @section('title','$okopedia - Show All Category')
 
 @section('container')
-    <<center>
-        <div class="admin_content">
-            <h3>Product</h3><br>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col">Product ID</th>
-                        <th scope="col">Product Image</th>
-                        <th scope="col">Product Name</th>
-                        <th scope="col">Product Category</th>
-                        <th scope="col">Product Price</th>
-                        <th scope="col">Product Description</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody> 
-                    @foreach($products as $p)
-                        <tr>
-                            <td>{{$p->id}}</td>
-                            <td><img src="{{$p->image}}" alt=""></td>
-                            <td>{{$p->name}}</td>
-                            <td>{{$p->category}}</td>
-                            <td>{{$p->price}}</td>
-                            <td>{{$p->description}}</td>
-                            <td><a href="#"><button type="button" class="btn btn-danger">Delete</button></a></td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+    <div class="container h-100">
+        <div class="row align-items-center h-100">
+            <div class="col-10 mx-auto">
+                <div class="admin_content">
+                    <h3>Product</h3><br>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">Product ID</th>
+                                <th scope="col">Product Image</th>
+                                <th scope="col">Product Name</th>
+                                <th scope="col">Product Category</th>
+                                <th scope="col">Product Price</th>
+                                <th scope="col">Product Description</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody> 
+                            @foreach($products as $p)
+                                <tr>
+                                    <td>{{$p->id}}</td>
+                                    <td><img src="{{$p->image}}" alt=""></td>
+                                    <td>{{$p->name}}</td>
+                                    <td>{{$p->category}}</td>
+                                    <td>{{$p->price}}</td>
+                                    <td>{{$p->description}}</td>
+                                    <td><a href="#"><button type="button" class="btn btn-danger">Delete</button></a></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-    </center>
+    </div>
 @endsection
 
 </body>
